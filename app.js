75119836089004a248acb3c42f2b77c958199bfe
Anup Subedi <anupsubedi87@gmail.com>
2023-09-01 20:29:01 +0545

    fix

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cors());
 
 // MongoDB Configuration
-mongoose.connect(process.env.MONGODB_URL)
+mongoose.connect('mongodb+srv://tripmate:1234@cluster0.oel14cc.mongodb.net/tripMate')
 mongoose.connection.on('error', err => {
     console.log('database connection failed');
 });
